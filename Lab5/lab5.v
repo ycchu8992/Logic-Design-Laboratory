b2a499be14f84f6872841b92428aeeb166ac08f6
ycchu8992 <ycchu8992@gmail.com>
2022-10-26 16:38:45 +0800

    Update lab5.v

--- Lab5/lab5.v
+++ Lab5/lab5.v
@@ -1,39 +1,3 @@
-module DEBOUNCE(
-	input wire clk,
-    input wire BTNR,
-    input wire BTNU,
-    input wire BTND,
-    input wire BTNL,
-	output wire pb_db_r,
-    output wire pb_db_u,
-    output wire pb_db_d,
-    output wire pb_db_l
-);
-
-	debounce btnr_db(.clk(clk), .pb(BTNR), .pb_debounced(pb_db_r));
-    debounce btnu_db(.clk(clk), .pb(BTNU), .pb_debounced(pb_db_u));
-    debounce btnd_db(.clk(clk), .pb(BTND), .pb_debounced(pb_db_d));
-    debounce btnl_db(.clk(clk), .pb(BTNL), .pb_debounced(pb_db_l));
-endmodule
-
-module ONEPULSE(
-	input wire clk,
-    input wire pb_in_r,
-    input wire pb_in_u,
-    input wire pb_in_d,
-    input wire pb_in_l,
-	output reg pb_out_r,
-    output reg pb_out_u,
-    output reg pb_out_d,
-    output reg pb_out_l
-);
-	one_pulse btnr_op(.clk(clk), .pb_in(pb_in_r), .pb_out(pb_out_r));    
-    one_pulse btnu_op(.clk(clk), .pb_in(pb_in_u), .pb_out(pb_out_u));
-    one_pulse btnd_op(.clk(clk), .pb_in(pb_in_d), .pb_out(pb_out_d)); 
-    one_pulse btnl_op(.clk(clk), .pb_in(pb_in_l), .pb_out(pb_out_l));
-endmodule
-
-
 module lab5(
     input wire clk,
     input wire rst,
@@ -59,7 +23,7 @@ module lab5(
 
 
     wire db_r, db_u, db_d, db_l;
-    reg btnr, btnu, btnd, btnl;
+    wire btnr, btnu, btnd, btnl;
     reg sec;
 
     reg [3:0] cnt, n_cnt;
@@ -74,38 +38,26 @@ module lab5(
 
 
     /*Real clock on fpga*/
-    //clock_divider #(.n(2**27)) sec_clk(.clk(clk), .clk_div(sec));
+    //clock_divider #(.n(2**27)) sec_clk(.clk(clk), .clk_div(t));
     //clock_divider #(.n(2**14)) display(.clk(clk), .clk_div(seg));
+    //always @(*) begin sec = t;end
     //always @(*) begin CLK = seg; end
 
     /*clock for simulation*/
-    clock_divider #(.n(2**5)) sec_clk(.clk(clk), .clk_div(sec));
+    clock_divider #(.n(2**5)) sec_clk(.clk(clk), .clk_div(t));
     clock_divider #(.n(2**3)) display(.clk(clk), .clk_div(seg));
+    always @(*) begin sec = t;end
     always @(*) begin CLK = seg; end
 
+    debounce btnr_db(.clk(clk), .pb(BTNR), .pb_debounced(db_r));
+    debounce btnu_db(.clk(clk), .pb(BTNU), .pb_debounced(db_u));
+    debounce btnd_db(.clk(clk), .pb(BTND), .pb_debounced(db_d));
+    debounce btnl_db(.clk(clk), .pb(BTNL), .pb_debounced(db_l));
 
-    DEBOUNCE DB(
-        .clk(clk),
-        .BTNR(BTNR),
-        .BTNU(BTNU),
-        .BTND(BTND),
-        .BTNL(BTNL),
-        .pb_db_r(db_r),
-        .pb_db_u(db_u),
-        .pb_db_d(db_d),
-        .pb_db_l(db_l)
-    );
-    ONEPULSE OP(
-        .clk(clk),
-        .pb_in_r(db_r),
-        .pb_in_u(db_u),
-        .pb_in_d(db_d),
-        .pb_in_l(db_l),
-        .pb_out_r(btnr),
-        .pb_out_u(btnu),
-        .pb_out_d(btnd),
-        .pb_out_l(btnl)
-    );
+    one_pulse btnr_op(.clk(clk), .pb_in(db_r), .pb_out(btnr));    
+    one_pulse btnu_op(.clk(clk), .pb_in(db_u), .pb_out(btnu));
+    one_pulse btnd_op(.clk(clk), .pb_in(db_d), .pb_out(btnd)); 
+    one_pulse btnl_op(.clk(clk), .pb_in(db_l), .pb_out(btnl));
 
     //state
     always @(posedge clk) begin
